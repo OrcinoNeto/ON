d3d8d1b475f450a7eafd0b88be0aa4f7a15cdc42
OrcinoNeto <129555010+OrcinoNeto@users.noreply.github.com>
2023-08-03 00:07:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  
+  return rect.bottom >= 0 && rect.top <= windowHeight;
+}
+
+function onScroll() {
+  const sections = document.querySelectorAll('.fade-in-left, .fade-in-right, .fade-in-topo, .fade-in-botton');
+  sections.forEach(section => {
+    if (isElementInViewport(section)) {
+      section.classList.add('fade-in');
+      section.classList.remove('fade-out');
+    } else {
+      section.classList.remove('fade-in');
+      section.classList.add('fade-out');
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', onScroll);
+document.addEventListener('scroll', onScroll);
+
+
+
